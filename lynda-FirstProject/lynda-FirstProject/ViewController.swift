092f9fcdb7cbd251d526fa99b4119bfb3fffa496
Richard Dizon <rcd6ab@labmini08.lab.cs.virginia.edu>
2016-09-24 16:51:22 -0400

    CH03 - starting todo app

--- lynda-FirstProject/lynda-FirstProject/ViewController.swift
+++ lynda-FirstProject/lynda-FirstProject/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var myTextField: UITextField!
     @IBOutlet weak var myLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // myTextField.delegate = self
     }
     @IBAction func didTapButton(_ sender: AnyObject) {
         // Use "var" for changes, use "let" if static
         let name = myTextField.text!
         myLabel.text = "Hi \(name)!"
+        myTextField.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        myTextField.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
