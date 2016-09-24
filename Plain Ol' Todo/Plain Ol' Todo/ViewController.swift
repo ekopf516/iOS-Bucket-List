531aa0d0dc2cf0633bc1553db521639ee3e0b890
Richard Dizon <rcd6ab@labmini08.lab.cs.virginia.edu>
2016-09-24 17:01:31 -0400

    CH03 - Completed Todo App

--- Plain Ol' Todo/Plain Ol' Todo/ViewController.swift
+++ Plain Ol' Todo/Plain Ol' Todo/ViewController.swift
@@ -12,12 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtInput: UITextField!
     @IBOutlet weak var txtOutput: UITextView!
 
+    var items:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     @IBAction func addItem(_ sender: AnyObject) {
+        if (txtInput.text! == "") {
+            return
+        }
+        items.append(txtInput.text!)
+        txtOutput.text = ""
+        for item in items {
+            txtOutput.text.append("\(item)\n")
+        }
+        txtInput.text = ""
+        txtInput.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
