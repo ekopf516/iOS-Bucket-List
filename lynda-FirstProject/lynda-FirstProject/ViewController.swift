0ab46e7fc1d7eef492d957d030516fd1ccbbf593
Richard Dizon <rcd6ab@labmini08.lab.cs.virginia.edu>
2016-09-24 15:48:37 -0400

    CH02 - handle a button press

--- lynda-FirstProject/lynda-FirstProject/ViewController.swift
+++ lynda-FirstProject/lynda-FirstProject/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func didTapButton(_ sender: AnyObject) {
         myLabel.text = "Changed with Code!"
     }
 
